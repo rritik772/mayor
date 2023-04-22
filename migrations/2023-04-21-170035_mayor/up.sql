94f993a2562abf02f28796a148e7772f7eaccc58
Ritik Ranjan <rritik772@outlook.com>
2023-04-22 13:44:52 +0530

    added diesel with sqlite database

---
+++ migrations/2023-04-21-170035_mayor/up.sql
@@ -0,0 +1,7 @@
+-- Your SQL goes here
+CREATE TABLE mayor (
+  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  specifier TEXT NOT NULL,
+  root_path TEXT NOT NULL,
+  file TEXT NOT NULL
+);
---
+++ src/schema.rs
@@ -0,0 +1,10 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    mayor (id) {
+        id -> Integer,
+        specifier -> Text,
+        root_path -> Text,
+        file -> Text,
+    }
+}
